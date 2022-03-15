d51c55e6289ee8c679a6b225bda477c63ce31ecc
zykat@yandex.ru <zykat@yandex.ru>
2022-03-15 17:26:41 +0900

    commit_2

---
+++ lesson_02.py
@@ -0,0 +1,15 @@
+''' 2.0 Write a program to print the "Python is cool!" string, use variable/s. '''
+str1 = 'Python is'
+str2 = 'cool'
+print(str1 + ' ' + str2)
+'''2.1 Write a program that adds two numbers, for numbers use variables, and print result.'''
+x = 5
+y = 19
+print(x+y)
+'''2.2 Write a program that subtracts two numbers, for numbers use variables, and print results.'''
+x = 5
+y = 19
+print(x-y)
+'''2.3 Write a program that prints type of variable.'''
+print(type(str2))
+print(type(y))

f0912bb16906a5914bf2b2b1117d5f162e6d3292
zykat@yandex.ru <zykat@yandex.ru>
2022-03-17 18:10:31 +0900

    add lesson_03.py

---
+++ lesson_03.py
@@ -0,0 +1,50 @@
+# # list my_list = []
+# my_list = [1, 2, "word", 4.0]
+# print(my_list)
+
+# a = 3
+# b = 7
+# print(a == b)
+
+# print(' '*4,'*')
+# print(' '*3,'***')
+# print(' '*2,'*****')
+# print(' '*1,'*******')
+
+# print(' '*4,'*'*1)
+# print(' '*3,'*'*3)
+# print(' '*2,'*'*5)
+# print(' '*1,'*'*7)
+
+# number = 4
+# zumber = 7
+# print(f"number -> {number},{zumber}")
+# print("number ->",  str(number) +","+ str(zumber))
+
+# a = 3
+# b = 5
+# def multiply(a, b):
+#    return(a * b)
+# print(multiply(a, b))
+
+# n = 4
+# def powers_of_two(n):
+#    return [2 ** x for x in range(n + 1)]
+# print(powers_of_two(n))
+
+name = 'Olga'
+name = 'Jack'
+
+# def return_name(name):
+#    return f'Hello, {name}, how are you?'
+# print(return_name(name))
+
+a = 10 % 3
+a = a + 2
+a +=3
+print(a)
+
+b = 5
+b /= 2
+print(b)
+
